d63d4df2a5fdae55dfb1b826dd2d451116c00645
Angelah Mbula Maweu <104361772+AngelahMbula@users.noreply.github.com>
2023-04-28 16:57:06 +0300

    Update 2-app.py

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -30,7 +30,7 @@ def get_locale() -> str:
 @app.route('/')
 def index() -> str:
     """Renders a basic html template."""
-    return render_template('1-index.html')
+    return render_template('2-index.html')
 
 
 if __name__ == '__main__':
